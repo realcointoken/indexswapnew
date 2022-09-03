f8b85c7044f1cc87c8064fa6d85181c25ef591b2
ufogreys <45027262+ufogreys@users.noreply.github.com>
2022-09-03 01:37:53 -0300

    Update index.ts

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -2,7 +2,7 @@ import { ChainId } from '@pancakeswap-libs/sdk'
 import MULTICALL_ABI from './abi.json'
 
 const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
-  [ChainId.MAINNET]: '0x1Ee38d535d541c55C9dae27B12edf090C608E6Fb', // TODO
+  [ChainId.MAINNET]: '0x17d06394bf05a7e1c681d87900638b19c8bc79ac', // TODO
   [ChainId.BSCTESTNET]: '0x301907b5835a2d723Fe3e9E8C5Bc5375d5c1236A'
 }
 
